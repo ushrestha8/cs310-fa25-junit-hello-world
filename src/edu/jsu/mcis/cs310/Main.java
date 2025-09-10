379b993538badac8164e4a54dd0ca8ad3856c58d
Utsav <shresthautsab345@gmail.com>
2025-09-10 11:28:00 -0500

    Completed Participation Assignment 2

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -17,7 +17,9 @@ public class Main {
     }
     
     public String reverse(String message) {
-        return message;
+        //StringBuilder
+        StringBuilder mg = new StringBuilder(message);
+        return mg.reverse().toString();
     }
     
 }
